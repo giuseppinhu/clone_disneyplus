21802a7f78415cbf3e0e5e67d8dc9b1995f33d46
Giuseppe <giuseppe.ribeiro.silva@gmail.com>
2025-01-24 21:23:22 -0300

    add 3 new sections and faq comments

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]');
+    const question = document.querySelectorAll('[data-faq-question]')
 
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(event) {
@@ -14,7 +15,20 @@ document.addEventListener('DOMContentLoaded', function() {
             event.target.classList.add('shows__tabs__button--is-active');
         });
     }
-});
+
+    for (let i = 0; i < question.length; i++) {
+        question[i].addEventListener('click', OpenClose);
+    }
+})
+
+function OpenClose(element) {
+    const class1 = 'faq__questions__item--is-open';
+    const elementDad = element.target.parentNode;
+    
+    elementDad.classList.toggle(class1)
+}
+
+
 
 function removeButtonActive() {
     const buttons = document.querySelectorAll('[data-tab-button]');
